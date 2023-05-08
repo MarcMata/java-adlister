9fed7049b00415d5c4eba57993971f9633e3864f
Marc Mata <marcmata26@gmail.com>
2023-05-08 12:12:54 -0500

    exercise complete

---
+++ src/main/java/CountServlet.java
@@ -0,0 +1,29 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Objects;
+
+@WebServlet(name = "CountServlet", urlPatterns = "/count")
+public class CountServlet extends HttpServlet {
+
+    private int count = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String reset = req.getParameter("reset");
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        if(reset != null) {
+            out.println("<h3>Count reset!</h3>");
+            out.println("<button><a href='/count'>Back to count</a></button>");
+            count = 0;
+
+        } else {
+            count++;
+            out.println("<h3>The count is " + count + "!</h3>");
+        }
+        }
+    }
--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,14 +1,22 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "HelloWorldServlet",urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        res.setContentType("text/html");
-        res.getWriter().println("<h1>Hello, World!</h1>");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String name = req.getParameter("name");
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        if(name != null) {
+            out.println("<h3>Ayyy! Hello, " + name + "!</h3>");
+        } else {
+            out.println("<h3>Tell me your name!</h3>");
+        }
     }
 }
