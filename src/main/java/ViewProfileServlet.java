dbd87fa3a4b44846a6bfef194a258d9df81a4644
Marc Mata <marcmata26@gmail.com>
2023-05-12 12:10:21 -0500

    accessing-data exercise walkthrough complete

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -3,11 +3,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Objects;
 
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+        HttpSession session = request.getSession();
+        boolean isLoggedIn = (boolean) session.getAttribute("isLoggedIn");
+        System.out.println("this equals " + isLoggedIn);
+        if (isLoggedIn){
+            request.getRequestDispatcher("/profile.jsp").forward(request, response);
+        } else {
+            response.sendRedirect("/login");
+        }
     }
 }
---
+++ src/main/webapp/logout.jsp
@@ -0,0 +1,17 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <jsp:include page="partials/head.jsp">
+        <jsp:param name="title" value="Please Log In" />
+    </jsp:include>
+</head>
+<body>
+<jsp:include page="partials/navbar.jsp" />
+
+    <h2>You have been successfully logged out</h2>
+    <button><a href="login">Login</a></button>
+
+
+</body>
+</html>
